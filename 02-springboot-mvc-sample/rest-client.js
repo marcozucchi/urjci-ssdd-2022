a3a6fec08e7ef84c4fccfe489663a656d660793b
marcozucchi <46170114+marcozucchi@users.noreply.github.com>
2022-05-20 21:44:36 +0200

    Añadidos ficheros propios de clase

---
+++ 02-springboot-mvc-sample/rest-client.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $.ajax({
+        url:"https://www.googleapis.com/books/v1/volumes?q=intitle:java"
+    }).done(function(data){
+        console.log(data);
+    })
+})
